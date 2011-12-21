5d4a36cfa108243fad9dd8e83b6d0c82839ac068
Patrick Debois <Patrick.Debois@jedi.be>
2011-12-21 18:06:25 +0100

    improved unix example to work

--- examples/unix.rb
+++ examples/unix.rb
@@ -1,8 +1,26 @@
 #!/usr/bin/env ruby
 
 require 'livestatus'
+require 'pp'
 
-c = Livestatus::Connection.new("unix:///var/nagios/rw/live")
+c = Livestatus::Connection.new({ :uri => "unix:///var/nagios/rw/live"})
 
 c.command("DISABLE_NOTIFICATIONS")
-puts c.get("status").inspect
+
+# This complains for missing table_name
+# puts c.get("status").inspect
+
+q_host=Livestatus::Host.new({})
+hosts=c.get(q_host)
+
+hosts.each do |host|
+  pp host
+end
+
+q_service=Livestatus::Service.new({})
+sercives=c.get(q_service)
+
+services.each do |service|
+  pp service
+end
+
